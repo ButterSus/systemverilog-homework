4ec8a7148935194e50fbb490019fbbfb06690da3
Krivoshapkin Eduard <buttersus@mail.ru>
2025-09-12 16:35:51 +0300

    feat: add solution 04_10

--- 04_arithmetics_and_pipelining/04_07_10_sqrt_formula_pipe/04_10_formula_2_pipe.sv
+++ 04_arithmetics_and_pipelining/04_07_10_sqrt_formula_pipe/04_10_formula_2_pipe.sv
@@ -42,5 +42,94 @@ module formula_2_pipe
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm#state_0
 
+    // We can't solve this task without revealing amount of stages in isqrt
+    // module, even though it's supposed to be "black box".
+
+    //----------------------------------------------------------------------------
+    // Stage 1: isqrt c
+    //----------------------------------------------------------------------------
+
+    logic        isqrt_c_vld;
+    logic [15:0] isqrt_c_res;
+
+    isqrt #(.n_pipe_stages(4)) i_isqrt_c
+    (
+        .clk   ( clk         ),
+        .rst   ( rst         ),
+        .x_vld ( arg_vld     ),
+        .x     ( c           ),
+        .y_vld ( isqrt_c_vld ),
+        .y     ( isqrt_c_res )
+    );
+
+    // Pipelined variables
+
+    logic [31:0] stage_1_a [0:3];
+    logic [31:0] stage_1_b [0:3];
+
+    always_ff @ (posedge clk) begin
+        if (arg_vld) begin
+            stage_1_a [0] <= a;
+            stage_1_b [0] <= b;
+        end
+
+        for (int i = 1; i < 4; i ++) begin
+            stage_1_a [i] <= stage_1_a [i - 1];
+            stage_1_b [i] <= stage_1_b [i - 1];
+        end
+    end
+
+    //----------------------------------------------------------------------------
+    // Stage 2: isqrt b
+    //----------------------------------------------------------------------------
+
+    logic        isqrt_bc_vld;
+    logic [15:0] isqrt_bc_res;
+
+    isqrt #(.n_pipe_stages(4)) i_isqrt_bc
+    (
+        .clk   ( clk                         ),
+        .rst   ( rst                         ),
+        .x_vld ( isqrt_c_vld                 ),
+        .x     ( stage_1_b [3] + isqrt_c_res ),
+        .y_vld ( isqrt_bc_vld                ),
+        .y     ( isqrt_bc_res                )
+    );
+
+    // Pipelined variables
+
+    logic [31:0] stage_2_a [0:3];
+
+    always_ff @ (posedge clk) begin
+        if (isqrt_c_vld) begin
+            stage_2_a [0] <= stage_1_a [3];
+        end
+
+        for (int i = 1; i < 4; i ++) begin
+            stage_2_a [i] <= stage_2_a [i - 1];
+        end
+    end
+
+    //----------------------------------------------------------------------------
+    // Stage 3: isqrt a
+    //----------------------------------------------------------------------------
+
+    logic        isqrt_abc_vld;
+    logic [15:0] isqrt_abc_res;
+
+    isqrt #(.n_pipe_stages(4)) i_isqrt_abc
+    (
+        .clk   ( clk                          ),
+        .rst   ( rst                          ),
+        .x_vld ( isqrt_bc_vld                 ),
+        .x     ( stage_2_a [3] + isqrt_bc_res ),
+        .y_vld ( isqrt_abc_vld                ),
+        .y     ( isqrt_abc_res                )
+    );
+
+    // Output logic
+
+    assign res = res_vld ? isqrt_abc_res : 'x;
+    assign res_vld = isqrt_abc_vld;
 
 endmodule
