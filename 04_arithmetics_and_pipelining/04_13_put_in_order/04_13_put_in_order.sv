a01fc87fbf4910ae2f8137cb6a3dfa364b550cca
Krivoshapkin Eduard <buttersus@mail.ru>
2025-09-13 22:24:47 +0300

    feat: add solution 04_13

--- 04_arithmetics_and_pipelining/04_13_put_in_order/04_13_put_in_order.sv
+++ 04_arithmetics_and_pipelining/04_13_put_in_order/04_13_put_in_order.sv
@@ -1,7 +1,8 @@
 module put_in_order
 # (
     parameter width    = 16,
-              n_inputs = 4
+              n_inputs = 4,
+              depth    = 2
 )
 (
     input                       clk,
@@ -11,8 +12,8 @@ module put_in_order
     input  [ n_inputs - 1 : 0 ]
            [ width    - 1 : 0 ] up_data,
 
-    output                      down_vld,
-    output [ width   - 1 : 0 ]  down_data
+    output logic                     down_vld,
+    output logic [ width   - 1 : 0 ] down_data
 );
 
     // Task:
@@ -30,5 +31,81 @@ module put_in_order
     // The idea of the block is kinda similar to the "parallel_to_serial" block
     // from Homework 2, but here block should also preserve the output order.
 
+    // Minimal FIFO implementation
+    logic [width-1:0] data_buffer [n_inputs][depth];
+    logic             data_valid  [n_inputs][depth];
+    
+    // Write pointers for each channel (where to store next incoming data)
+    logic [$clog2(depth)-1:0] wr_ptr [n_inputs];
+    // Read pointers for each channel (which buffer to read next)
+    logic [$clog2(depth)-1:0] rd_ptr [n_inputs];
+    
+    // One-hot: which channel we're waiting for next
+    logic [n_inputs-1:0] channel_idx;
+    
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            channel_idx <= 1'b1;  // One-hot: waiting for input[0]
+
+            for (int i = 0; i < n_inputs; i++) begin
+                wr_ptr[i] <= '0;
+                rd_ptr[i] <= '0;
+                for (int j = 0; j < depth; j++) begin
+                    data_valid[i][j] <= 1'b0;
+                end
+            end
+        end else begin
+            // Store incoming data in buffers
+            for (int i = 0; i < n_inputs; i++) begin
+                if (up_vlds[i]) begin
+                    // Store in the next available buffer slot
+                    data_buffer[i][wr_ptr[i]] <= up_data[i];
+                    data_valid[i][wr_ptr[i]]  <= 1'b1;
+                    
+                    // Advance write pointer (circular)
+                    if (wr_ptr[i] == depth - 1)
+                        wr_ptr[i] <= '0;
+                    else
+                        wr_ptr[i] <= wr_ptr[i] + 1;
+                end
+            end
+            
+            // Check if we can output the next expected result
+            for (int i = 0; i < n_inputs; i++) begin
+                if (channel_idx[i] && data_valid[i][rd_ptr[i]]) begin
+                    // Mark current buffer slot as consumed
+                    data_valid[i][rd_ptr[i]] <= 1'b0;
+                    
+                    // Advance read pointer (circular)
+                    if (rd_ptr[i] == depth - 1)
+                        rd_ptr[i] <= '0;
+                    else
+                        rd_ptr[i] <= rd_ptr[i] + 1;
+                    
+                    // Move to next channel in round-robin fashion
+                    if (i == n_inputs - 1) begin
+                        channel_idx <= 1'b1;
+                    end else begin
+                        channel_idx <= channel_idx << 1;
+                    end
+                end
+            end
+        end
+    end
+    
+    // Output logic - combinational
+    always_comb begin
+        down_vld  = 1'b0;
+        down_data = '0;
+        
+        // Check if the data we're waiting for is available
+        // Since channel_idx is one-hot, only one condition can be true
+        for (int i = 0; i < n_inputs; i++) begin
+            if (channel_idx[i] && data_valid[i][rd_ptr[i]]) begin
+                down_vld  = 1'b1;
+                down_data = data_buffer[i][rd_ptr[i]];
+            end
+        end
+    end
 
 endmodule
