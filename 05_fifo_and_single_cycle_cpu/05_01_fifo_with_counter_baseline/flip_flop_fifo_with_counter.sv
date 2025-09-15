e385f6092d77f170d20c3d33cd8ce9db77634258
Krivoshapkin Eduard <buttersus@mail.ru>
2025-09-15 18:27:29 +0300

    feat: add pseudo-solution 05_01

--- 05_fifo_and_single_cycle_cpu/05_01_fifo_with_counter_baseline/flip_flop_fifo_with_counter.sv
+++ 05_fifo_and_single_cycle_cpu/05_01_fifo_with_counter_baseline/flip_flop_fifo_with_counter.sv
@@ -17,33 +17,50 @@ module flip_flop_fifo_with_counter
     output               full
 );
 
+    // Even though this is not task, I just discovered
+    // that counter here is used only to reduce output latency after posedge.
+    // So we can remove counter completely just for fun!
+
     //------------------------------------------------------------------------
 
-    localparam pointer_width = $clog2 (depth),
-               counter_width = $clog2 (depth + 1);
-
+    localparam pointer_width = $clog2 (depth);
     localparam max_ptr = pointer_width' (depth - 1);
 
     //------------------------------------------------------------------------
 
-    logic [pointer_width - 1:0] wr_ptr, rd_ptr;
-    logic [counter_width - 1:0] cnt;
+    logic [pointer_width - 1:0] wr_ptr, wr_ptr_next;
+    logic [pointer_width - 1:0] rd_ptr, rd_ptr_next;
 
     logic [width - 1:0] data [0: depth - 1];
 
+    logic empty_flag;
+
     //------------------------------------------------------------------------
+
+    always_comb begin
+        wr_ptr_next = wr_ptr == max_ptr ? '0 : wr_ptr + 1'b1;
+        rd_ptr_next = rd_ptr == max_ptr ? '0 : rd_ptr + 1'b1;
+    end
 
     always_ff @ (posedge clk or posedge rst)
         if (rst)
             wr_ptr <= '0;
         else if (push)
-            wr_ptr <= wr_ptr == max_ptr ? '0 : wr_ptr + 1'b1;
+            wr_ptr <= wr_ptr_next;
 
     always_ff @ (posedge clk or posedge rst)
         if (rst)
             rd_ptr <= '0;
         else if (pop)
-            rd_ptr <= rd_ptr == max_ptr ? '0 : rd_ptr + 1'b1;
+            rd_ptr <= rd_ptr_next;
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+            empty_flag <= 1'b1;
+        else if (pop)
+            empty_flag <= rd_ptr_next == wr_ptr;
+        else if (push)
+            empty_flag <= 1'b0;
 
     //------------------------------------------------------------------------
 
@@ -55,17 +72,7 @@ module flip_flop_fifo_with_counter
 
     //------------------------------------------------------------------------
 
-    always_ff @ (posedge clk or posedge rst)
-        if (rst)
-            cnt <= '0;
-        else if (push & ~ pop)
-            cnt <= cnt + 1'b1;
-        else if (pop & ~ push)
-            cnt <= cnt - 1'b1;
-
-    //------------------------------------------------------------------------
-
-    assign empty = (cnt == '0);  // Same as "~| cnt"
-    assign full  = (cnt == counter_width' (depth) );
+    assign empty = (wr_ptr == rd_ptr) &  empty_flag;
+    assign full  = (wr_ptr == rd_ptr) & ~empty_flag;
 
 endmodule
