0af1195fd9401067bc8135d676bb5280a1263788
Krivoshapkin Eduard <buttersus@mail.ru>
2025-09-16 17:28:05 +0300

    feat: add solution 05_06

--- 05_fifo_and_single_cycle_cpu/05_06_sqrt_formula_pipe/05_06_formula_2_pipe_using_fifos.sv
+++ 05_fifo_and_single_cycle_cpu/05_06_sqrt_formula_pipe/05_06_formula_2_pipe_using_fifos.sv
@@ -3,6 +3,10 @@
 //----------------------------------------------------------------------------
 
 module formula_2_pipe_using_fifos
+# (
+    // Amount of stages per isqrt module
+    parameter N = 4
+)
 (
     input         clk,
     input         rst,
@@ -41,5 +45,92 @@ module formula_2_pipe_using_fifos
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm
 
+    //----------------------------------------------------------------------------
+    // Stage 1: isqrt c
+    //----------------------------------------------------------------------------
+
+    logic        isqrt_c_vld;
+    logic [15:0] isqrt_c_res;
+
+    isqrt #(.n_pipe_stages(N)) i_isqrt_c
+    (
+        .clk   ( clk         ),
+        .rst   ( rst         ),
+        .x_vld ( arg_vld     ),
+        .x     ( c           ),
+        .y_vld ( isqrt_c_vld ),
+        .y     ( isqrt_c_res )
+    );
+
+    // Pipelined variables
+
+    logic [31:0] fifo_b_data;
+
+    flip_flop_fifo_with_counter #(.width(32), .depth(N)) i_fifo_b
+    (
+        .clk        ( clk         ),
+        .rst        ( rst         ),
+        .push       ( arg_vld     ),
+        .pop        ( isqrt_c_vld ),
+        .write_data ( b           ),
+        .read_data  ( fifo_b_data )
+        // .empty      (),
+        // .full       (),
+    );
+
+    //----------------------------------------------------------------------------
+    // Stage 2: isqrt b
+    //----------------------------------------------------------------------------
+
+    logic        isqrt_bc_vld;
+    logic [15:0] isqrt_bc_res;
+
+    isqrt #(.n_pipe_stages(N)) i_isqrt_bc
+    (
+        .clk   ( clk                       ),
+        .rst   ( rst                       ),
+        .x_vld ( isqrt_c_vld               ),
+        .x     ( fifo_b_data + isqrt_c_res ),
+        .y_vld ( isqrt_bc_vld              ),
+        .y     ( isqrt_bc_res              )
+    );
+
+    // Pipelined variables
+
+    logic [31:0] fifo_a_data;
+
+    flip_flop_fifo_with_counter #(.width(32), .depth(2 * N)) i_fifo_a
+    (
+        .clk        ( clk          ),
+        .rst        ( rst          ),
+        .push       ( arg_vld      ),
+        .pop        ( isqrt_bc_vld ),
+        .write_data ( a            ),
+        .read_data  ( fifo_a_data  )
+        // .empty      (),
+        // .full       (),
+    );
+
+    //----------------------------------------------------------------------------
+    // Stage 3: isqrt a
+    //----------------------------------------------------------------------------
+
+    logic        isqrt_abc_vld;
+    logic [15:0] isqrt_abc_res;
+
+    isqrt #(.n_pipe_stages(N)) i_isqrt_abc
+    (
+        .clk   ( clk                        ),
+        .rst   ( rst                        ),
+        .x_vld ( isqrt_bc_vld               ),
+        .x     ( fifo_a_data + isqrt_bc_res ),
+        .y_vld ( isqrt_abc_vld              ),
+        .y     ( isqrt_abc_res              )
+    );
+
+    // Output logic
+
+    assign res = res_vld ? isqrt_abc_res : 'x;
+    assign res_vld = isqrt_abc_vld;
 
 endmodule
